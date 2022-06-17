9a2e03c36e6e546a46ac6de23edc385e6e067268
arthurlba1 <1arthurluiz1@gmail.com>
2022-06-17 19:29:00 -0300

    Initial architecture state

---
+++ src/test/java/management/people/people_management_api/PeopleManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package management.people.people_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
