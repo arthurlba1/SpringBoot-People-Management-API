0ce18ad82d2cd129ad014fbf10f4b6d7809a960a
arthurlba1 <1arthurluiz1@gmail.com>
2022-08-22 21:56:19 -0300

    refactor rest class

--- src/main/java/management/people/people_management_api/controller/PersonController.java
+++ src/main/java/management/people/people_management_api/controller/PersonController.java
@@ -1,33 +1,37 @@
 package management.people.people_management_api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import management.people.people_management_api.dto.MessageResponseDTO;
 import management.people.people_management_api.entity.Person;
-import management.people.people_management_api.repository.PersonRepository;
+import management.people.people_management_api.service.PersonService;
 
-
+/*
+ * Control class from person to REST layer.
+ */
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    private PersonRepository personRepository;
+ private PersonService personService;
 
-    @Autowired
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
+ @Autowired
+ public PersonController(PersonService personService) {
+    this.personService = personService;
+ }
 
-    @PostMapping
-    public MessageResponseDTO createPerson(@RequestBody Person person){
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID (CPF) " + savedPerson.getCpf())
-                .build();
-    }
+ /*
+  * Method to create a new person.
+  */
+ @PostMapping
+ @ResponseStatus(HttpStatus.CREATED)
+ public MessageResponseDTO createPerson (@RequestBody Person person) {
+    return personService.createPerson(person);
+ }
 }
